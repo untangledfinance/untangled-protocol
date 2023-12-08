df74c083990c334cba19368e927e25d1fb87ca0e
Luu Minh Tan <tanlm@bravezone.com>
2023-12-08 17:46:08 +0700

    SecuritizationTranch: update test

--- contracts/protocol/pool/ISecuritizationTranche.sol
+++ contracts/protocol/pool/ISecuritizationTranche.sol
@@ -12,10 +12,16 @@ interface ISecuritizationTranche {
     /// @param newRedeemAmount new amount of tokens to be redeemed
     function redeemSOTOrder(uint256 newRedeemAmount) external;
 
+    /// @notice Pause redeem request
     function setRedeemDisabled(bool _redeemDisabled) external;
 
+    /// @notice Total amount of SOT redeem order
     function totalSOTRedeem() external view returns (uint256);
+    /// @notice Total amount of JOT redeem order
     function totalJOTRedeem() external view returns (uint256);
+    /// @dev Retrieves the amount of JOT tokens that can be redeemed for the specified user.
+    /// @param usr The address of the user for which to retrieve the redeemable JOT amount.
+    /// @return The amount of JOT tokens that can be redeemed by the user.
     function userRedeemJOTOrder(address usr) external view returns (uint256);
     function userRedeemSOTOrder(address usr) external view returns (uint256);
 }

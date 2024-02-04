5edb28b351f260e037d8b1da6e4577deb3c11a4d
duongnd <duong.tnhp8386@gmail.com>
2024-02-04 23:29:33 +0700

    feat(config): viaIR mode disable

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,7 +26,7 @@ module.exports = {
                         enabled: true,
                         runs: 200,
                     },
-                    viaIR: true
+                    // viaIR: true
                 },
             },
         ],

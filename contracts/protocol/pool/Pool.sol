b6076d8b60f52c55401802a13adb3b26c3826a00
huyminh1115 <huyminh1115@gmail.com>
2024-03-26 02:53:59 +0700

    Update

--- contracts/protocol/pool/Pool.sol
+++ contracts/protocol/pool/Pool.sol
@@ -261,6 +261,9 @@ contract Pool is IPool, PoolStorage, UntangledBase {
 
     function updateAssetRiskScore(bytes32 nftID_, uint256 risk_) external onlyRole(POOL_ADMIN_ROLE) {
         PoolNAVLogic.updateAssetRiskScore(_poolStorage, nftID_, risk_);
+
+        // rebase
+        rebase();
     }
 
     /// @notice retrieves loan information
@@ -296,6 +299,13 @@ contract Pool is IPool, PoolStorage, UntangledBase {
             'SecuritizationPool: Caller must be SecuritizationManager or NoteTokenVault'
         );
         TGELogic.increaseCapitalReserve(_poolStorage, currencyAmount);
+    }
+
+    function contributeToCapitalReserve(uint256 amount) external whenNotPaused {
+        TGELogic.contributeToCapitalReserve(_poolStorage, msg.sender, amount);
+
+        // rebase
+        rebase();
     }
 
     /// @dev trigger update reserve

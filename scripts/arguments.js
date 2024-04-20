5d397429829e1be8a1d50289d4a8ef493cd86c19
vjtc0n <vjtc0n12345@gmail.com>
2024-04-20 20:04:41 +0700

    Add arguments for LAT verification

---
+++ scripts/arguments.js
@@ -0,0 +1,3 @@
+module.exports = ['0x372b4Df3E6EAf8e2e825B266B4E89fce90CEf152'];
+
+// npx hardhat verify --network celo 0x5147C3476E675FBDbFeF8E2576A5bDe20b438B0B --constructor-args scripts/arguments.js
